37f5638d6788bd0aaac515562194e3b8971a32e7
blizx <1194474469@qq.com>
2024-11-12 18:48:49 +0800

    feat: 简单Layout布局

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,18 @@
+
+
+<template>
+  <router-view/>
+</template>
+
 <script setup lang="ts">
 </script>
 
-<template>
-  <el-button type="primary">111</el-button>
-  <router-view/>
-
-</template>
-
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
 }
 </style>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,17 +1,21 @@
 import {createRouter, createWebHashHistory} from 'vue-router';
-import HelloWorld  from '@/components/HelloWorld.vue';
+import Layout from '@/layout/index.vue'
 
 const router = createRouter({
   history: createWebHashHistory(),
   routes: [
     {
       path: '/',
-      redirect: '/home'
-    },
-    {
-      path: '/home',
-      name: 'Home',
-      component: HelloWorld
+      redirect: '/dashboard',
+      component: Layout,
+      children: [
+        {
+          path: '/dashboard',
+          name: 'Dashboard',
+          component: () => import('@/views/dashboard/index.vue'),
+          meta: {title: '首页', icon: 'dashboard', affix: true}
+        },
+      ]
     },
   ]
 })
